b7fee2e009850ac85e8dda98f18eb85cc7d353ff
Tzuyu's Forgotten Luggage <84631221+TzuyusForgottenLuggage@users.noreply.github.com>
2021-05-23 10:58:15 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,16 @@
+#Simple Program to display output using different datatypes learnt in day2 of class.
+
+name='Kishan'
+age=21
+per=88.80
+lang=True
+scount=6
+ucount=9
+
+print('Hello! I am {}.I am {} years old.I have secured {}% in 10th.I love listening to various genres of music but my ults are TWICE , Nicki Minaj & BTS.Yes, it is {} that i am multilingual. I can speak in {}+ languages and understand more than {} languages !!!'.format(name,age,per,lang,scount,ucount))
+
+#Displaying same statement with newline(\n) as seperators
+
+print("\n\nDisplaying same statement with newline char as seperators\n\n")
+
+print('Hello! I am',name,'.\nI am',age,'years old.\nI have secured ',per,'% in 10th.\nI love listening to various genres of music but my ults are TWICE , Nicki Minaj & BTS.\nYes, it is',lang,' that i am multilingual.\nI can speak in ',scount,'+ languages and understand more than ',ucount,'languages !!!')
