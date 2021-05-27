cf0e7ce4ca4920d8ef7e1b425d9399befe38a6be
Tzuyu's Forgotten Luggage <84631221+TzuyusForgottenLuggage@users.noreply.github.com>
2021-05-27 15:30:49 +0530

    Add files via upload

---
+++ Day3-2.py
@@ -0,0 +1,11 @@
+#simple program using various operators in python
+a=int(input('\nenter first number : '))
+b=int(input('\nenter second number : '))
+print('\nAddition is ',a+b)
+print("\nSubtraction is ",a-b)
+print("\nProduct is ",a*b)
+print('\n(Division using / )Quotient on divison is ',a/b)
+print('\n(Division using // )Nearest whole number on division is ',a//b)
+print('\n(Division using % )Remainder on divison is ',a%b)
+print('\nPower of',a,'raised to',b,' is ',a**b)
+
