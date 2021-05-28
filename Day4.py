312bf69c3c83566341eda850b9aff88026f121f4
Tzuyu's Forgotten Luggage <84631221+TzuyusForgottenLuggage@users.noreply.github.com>
2021-05-28 12:11:25 +0530

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,14 @@
+#Counting the occurance of letter y in given word using string functions
+
+a=input('Enter a string in which to Count the occurance of letter y \n')
+print(a.count('y'))
+
+#printing even indexed characters of given input string
+
+b=input('Enter a string to print even indexed characters of.\n')
+print(b[: :2])
+
+#swapcase on given string
+
+c=input('Enter a string to impose swapcase on.\n')
+print(c.swapcase())
